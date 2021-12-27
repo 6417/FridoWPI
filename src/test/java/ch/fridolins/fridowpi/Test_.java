be744b5437436291fd9cef9244b007aa7e9c7481
Gian-Laager <gianlaager114@gmail.com>
2021-12-27 10:13:33 +0100

    setup project with junit and wpilib

---
+++ src/test/java/ch/fridolins/fridowpi/Test_.java
@@ -0,0 +1,12 @@
+package ch.fridolins.fridowpi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Test_ {
+    @Test
+    public void helloJunit() {
+        assertTrue(true);
+    }
+}
