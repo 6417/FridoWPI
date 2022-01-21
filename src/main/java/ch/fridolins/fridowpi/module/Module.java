e44d04184289e1fb8b06f7cb59dbdda3293395dd
Gian-Laager <gianlaager114@gmail.com>
2022-01-21 16:16:13 +0100

    fixed merge conflicts

--- src/main/java/ch/fridolins/fridowpi/module/Module.java
+++ src/main/java/ch/fridolins/fridowpi/module/Module.java
@@ -2,12 +2,16 @@ package ch.fridolins.fridowpi.module;
 
 import ch.fridolins.fridowpi.Initializer;
 import ch.fridolins.fridowpi.base.OptionalInitialisable;
+import ch.fridolins.fridowpi.joystick.Binding;
+import ch.fridolins.fridowpi.joystick.JoystickBindable;
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class Module extends SubsystemBase implements OptionalInitialisable, IModule {
+public class Module extends SubsystemBase implements JoystickBindable, OptionalInitialisable, IModule {
     private Set<IModule> submodules = new HashSet<>();
 
     public Module() {
@@ -16,7 +20,9 @@ public class Module extends SubsystemBase implements OptionalInitialisable, IMod
 
     @Override
     public void registerSubmodule(IModule... modules) {
-        Arrays.stream(modules).forEach((module) -> {assert (getAllSubModules().stream().noneMatch((other) -> this == other) && module != this) : "'this' can not be a submodule of its self";});
+        Arrays.stream(modules).forEach((module) -> {
+            assert (getAllSubModules().stream().noneMatch((other) -> this == other) && module != this) : "'this' can not be a submodule of its self";
+        });
         submodules.addAll(Set.of(modules));
     }
 
