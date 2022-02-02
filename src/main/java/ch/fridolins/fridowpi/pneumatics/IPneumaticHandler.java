c99663ad15c9945e555ac87939287a08fd9fde1f
Ambros <ambros.anrig@gmail.com>
2022-02-02 16:04:22 +0100

    Add Pneumatic System

---
+++ src/main/java/ch/fridolins/fridowpi/pneumatics/IPneumaticHandler.java
@@ -0,0 +1,17 @@
+package ch.fridolins.fridowpi.pneumatics;
+
+import ch.fridolins.fridowpi.base.Initialisable;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
+public interface IPneumaticHandler extends Initialisable, Sendable {
+    void enableCompressor();
+
+    void disableCompressor();
+
+    void configureCompressor(int id, PneumaticsModuleType pcmType);
+
+    int getCompressorId();
+
+    PneumaticsModuleType getCompressorType();
+}
