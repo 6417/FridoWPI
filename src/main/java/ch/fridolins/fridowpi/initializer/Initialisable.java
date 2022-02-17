952b0c2e25db8f5c12ce8265d9916cdd9a94c04c
Gian-Laager <gianlaager114@gmail.com>
2022-02-17 17:56:02 +0100

    added docs to Initialisable

--- src/main/java/ch/fridolins/fridowpi/initializer/Initialisable.java
+++ src/main/java/ch/fridolins/fridowpi/initializer/Initialisable.java
@@ -5,10 +5,20 @@ import java.util.LinkedList;
 import java.util.List;
 
 public interface Initialisable {
+    /**
+     * In this function all the objects that depend on some other {@link Initialisable} are instantiated
+     */
     void init();
 
+    /**
+     * @return if everything initialized
+     */
     boolean isInitialized();
 
+    /**
+     * Add a dependency, this means {@code initialisable} will be initialized before the current object
+     * @param initialisable will be initialized before {@code this}, may not be {@code this}
+     */
     default void requires(Initialisable initialisable) {
         Initializer.getInstance().after(initialisable, this);
     }
